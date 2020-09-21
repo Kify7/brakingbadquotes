3c7fc4d4eaf9ba1f1ab15b2734f9be36cc880240
Kify <kflores.valona@gmail.com>
2020-09-20 22:59:39 -0500

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,39 +1,59 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import Frase from "./components/Frase";
 
 const Contenedor = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
   padding-top: 5rem;
-`
+`;
 
 const Boton = styled.button`
-  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0F574e 100%);
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
   background-size: 300px;
   font-family: Arial, Helvetica, sans-serif;
-  color: #FFF;
+  color: #fff;
   margin-top: 3rem;
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size .8s ease;
 
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `;
 
 function App() {
+  // state de frases
+  const [frase, guardarFrase] = useState({});
 
-  const consultarAPI = () =>{
-    console.log('consultando...');
-  }
+  const consultarAPI = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+    guardarFrase(frase[0]);
+  };
+
+  // cargar una frase
+  useEffect( () => {
+    consultarAPI()
+  }, []);
   return (
     <Contenedor>
-      <Boton
-      onClick={() => consultarAPI()}
-      >
-      Quote for me
-    </Boton>
+      <Frase
+        frase={frase}
+      />
+      <Boton onClick={() => consultarAPI()}>Quote for me</Boton>
     </Contenedor>
-    
   );
 }
 
